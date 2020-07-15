b70525cc2228f82755e18ccf81865c40961e5ab1
rizkypsr <rizkypsrr@gmail.com>
2020-07-15 21:00:09 +0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const hamburgerBtnMoved = document.querySelector("#hamburgerBtn.move");
 const navbar = document.querySelector(".navbar");
 
 hamburgerBtn.addEventListener("click", showNavbar);
 
 function showNavbar() {
-  hamburgerBtn.classList.toggle("move-hamburger");
-  hamburgerBtn.classList.toggle("fade-in");
+  moveHamburgerPos();
 
   if (hamburgerBtn.getAttribute("src") === "img/icons8-menu-30.png") {
     hamburgerBtn.setAttribute("src", "img/icons8-multiply-24.png");
@@ -14,4 +14,17 @@ function showNavbar() {
   }
 
   navbar.classList.toggle("navbar-active");
+}
+
+function moveHamburgerPos() {
+  const windowWidth = window.innerWidth;
+  const navbarWidth = navbar.offsetWidth;
+
+  const posLeft = `${(navbarWidth - windowWidth).toString()}px`;
+
+  if (hamburgerBtn.style.left === "") {
+    hamburgerBtn.style.left = posLeft;
+  } else {
+    hamburgerBtn.style.left = "";
+  }
 }
